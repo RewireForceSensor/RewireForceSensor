bbece64d1acfb767c47543e2ccbbf5ee5711af91
DeCapo-1 <corbin.capo@utexas.edu>
2021-08-03 15:20:03 -0500

    Now I have SDK

--- V2-Android-App/app/src/main/java/com/example/rewirebluetoothforcesensor/MainActivity.java
+++ V2-Android-App/app/src/main/java/com/example/rewirebluetoothforcesensor/MainActivity.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
+import jheatchart-0.6.jar;
 
 import static android.content.ContentValues.TAG;
 
