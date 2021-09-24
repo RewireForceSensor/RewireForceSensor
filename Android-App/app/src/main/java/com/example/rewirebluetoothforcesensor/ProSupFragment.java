34c59c38e1d5e412ccb4e6908530f07a49542e7f
IshDeshpa <ishdeshpa@gmail.com>
2021-09-24 12:25:59 -0500

    configured some chart settings to make it look better. chart is working but still needs more configuration and a second data set.

--- Android-App/app/src/main/java/com/example/rewirebluetoothforcesensor/ProSupFragment.java
+++ Android-App/app/src/main/java/com/example/rewirebluetoothforcesensor/ProSupFragment.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ProSupFragment extends DataViewFragment {
-    int[] sensorDataArr;
+    double[] sensorDataArr;
     int totalCycles;
 
     LineChart chart;
@@ -38,7 +38,7 @@ public class ProSupFragment extends DataViewFragment {
         chart = (LineChart) rootView.findViewById(R.id.chart);
 
         List<Entry> entries = new ArrayList<Entry>();
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        LineDataSet dataSet = createSet();
         LineData data = new LineData(dataSet);
 
         chart.setData(data);
@@ -49,35 +49,40 @@ public class ProSupFragment extends DataViewFragment {
 
     public void putSensorData(Bundle args){
         for(int i=0; i<6; i++) {
-            sensorDataArr = args.getIntArray("sensorData");
+            sensorDataArr = args.getDoubleArray("sensorData");
             totalCycles = args.getInt("totalCycles");
         }
     }
 
     public void update(){
-//        LineData data = chart.getData();
-//
-//        if (data != null) {
-//
-//            ILineDataSet dataSet = data.getDataSetByIndex(0);
-//            // set.addEntry(...); // can be called as well
-//
-//            if (dataSet == null) {
-//                dataSet = createSet();
-//                data.addDataSet(dataSet);
-//            }
-//
-//            data.addEntry(new Entry(dataSet.getEntryCount(), sensorDataArr[1] - sensorDataArr[2]), 0);
-//            data.notifyDataChanged();
-//
-//            // let the chart know it's data has changed
-//            chart.notifyDataSetChanged();
-//
-//            chart.setVisibleXRangeMaximum(120);
-//
-//            chart.moveViewToX(data.getEntryCount());
-//
-//        }
+        LineData data = chart.getData();
+
+        if (data != null) {
+
+            ILineDataSet dataSet = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (dataSet == null) {
+                dataSet = createSet();
+                data.addDataSet(dataSet);
+            }
+
+            data.addEntry(new Entry(dataSet.getEntryCount(), (float)(sensorDataArr[1] - sensorDataArr[2])), 0); // Left side
+            //dataSet.getEntryCount();
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            chart.notifyDataSetChanged();
+
+            chart.setVisibleXRangeMaximum(120);
+            chart.getAxisLeft().setAxisMaximum(10f);
+            chart.getAxisLeft().setAxisMinimum(-10f);
+            chart.getAxisRight().setEnabled(false);
+            //chart.setVisibleYRangeMaximum(50f, YAxis.AxisDependency.LEFT);
+
+            chart.moveViewToX(data.getEntryCount());
+
+        }
     }
 
 
@@ -86,15 +91,17 @@ public class ProSupFragment extends DataViewFragment {
         LineDataSet dataSet = new LineDataSet(null, "Dynamic Data");
         dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
         dataSet.setColor(ColorTemplate.getHoloBlue());
-        dataSet.setCircleColor(Color.WHITE);
-        dataSet.setLineWidth(2f);
-        dataSet.setCircleRadius(4f);
+        //dataSet.setCircleColor(Color.BLACK);
+        dataSet.setLineWidth(3f);
+        //dataSet.setCircleRadius(4f);
+        dataSet.setDrawCircles(false);
         dataSet.setFillAlpha(65);
         dataSet.setFillColor(ColorTemplate.getHoloBlue());
         dataSet.setHighLightColor(Color.rgb(244, 117, 117));
         dataSet.setValueTextColor(Color.WHITE);
         dataSet.setValueTextSize(9f);
         dataSet.setDrawValues(false);
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER); // Makes lines smooth
         return dataSet;
     }
 }
