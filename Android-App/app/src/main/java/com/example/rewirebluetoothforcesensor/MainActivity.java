693da95a386feced4a3fdc435967fd4a835e5463
Ishan Deshpande <ishdeshpa@gmail.com>
2022-04-18 16:24:15 -0500

    Add center of pressure (#24)
    * added progress bar
    
    * made significant progress on center of pressure. conversion to absolute coordinates is still crappy because of the clamping, but it works OK ish
    
    * worked on fixing COP
    
    * added floor so that data cannot go below 0.

--- Android-App/app/src/main/java/com/example/rewirebluetoothforcesensor/MainActivity.java
+++ Android-App/app/src/main/java/com/example/rewirebluetoothforcesensor/MainActivity.java
@@ -369,7 +369,7 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         }
 
-                        sensorDataArr = new double[8];
+                        sensorDataArr = new double[10];
 
                         for(int i=0; i<sensorDataArr.length; i++){
                             //Log.e("VALUE", splitArr[i]);
@@ -381,7 +381,9 @@ public class MainActivity extends AppCompatActivity {
                                 skipRead = true;
                                 break;
                             }
+                        }
 
+                        for(int i=0; i<8; i++){
                             if(calibrationStage == CalibrationStage.OFFSET){
                                 // Add to average offset
                                 offsets[i] *= (totalCycles-offsetCycleStart-1);
@@ -401,7 +403,8 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                             sensorDataArr[i] /= factors[i];
-                            sensorDataArr[i] = Math.floor(sensorDataArr[i] * 100)/100;
+                            sensorDataArr[i] = Math.max(0, Math.floor(sensorDataArr[i] * 100)/100);
+
                         }
 
                         Log.i("factors", Arrays.toString(factors));
